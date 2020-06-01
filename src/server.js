2cf0d6053af86db136cc0f41daa4e7977569e202
Gabriel <gabriel.alves450@hotmail.com>
2020-06-01 16:46:35 -0300

    Procfile e prod

--- src/server.js
+++ src/server.js
@@ -13,4 +13,4 @@ app.use((error, req, res, next)=>{
     res.json({error: error.message})
 })
 
-app.listen(3333, () => console.log('Server is running'))
+app.listen(precess.env.PORT || 3000, () => console.log('Server is running'))
