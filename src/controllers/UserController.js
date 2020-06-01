a226a950654c89cadf78178dcf14d6450a91f97a
Gabriel <gabriel.alves450@hotmail.com>
2020-06-01 17:55:35 -0300

    fixed bugs proc

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,7 +2,7 @@ const knex = require('../database')
 
 module.exports = {
     async index(req, res) {
-        const results = await knex('/')
+        const results = await knex('/players')
         return res.json(results)
     },
 
