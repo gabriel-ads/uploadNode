5d2403e2a1f7842f2a0893c1070eb836c63fede4
Gabriel <gabriel.alves450@hotmail.com>
2020-06-01 13:38:31 -0300

    Backend quase pronto, cadastra e loga user. Subindo para prod

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,21 +5,38 @@ module.exports = {
         const results = await knex('players')
         return res.json(results)
     },
+
+    async logon(req, res,next) {
+        try {
+            const { email, password } = req.body
+
+            const results = await knex('players').where({
+                email , password
+            })
+            return res.json(results)
+
+        } catch (error) {
+            next(error)
+        }
+        
+    },
+
     async questions(req, res, next) {
         const results = await knex('questions')
         return res.json(results)
     },
-    // async createPlayer(req, res, next) {
-    //     try {
-    //         const { nickname, email, password } = req.body
 
-    //         await knex('players').insert({
-    //             nickname, email, password
-    //         })
+    async createPlayer(req, res, next) {
+        try {
+            const { nickname, email, password } = req.body
+
+            await knex('players').insert({
+                nickname, email, password
+            })
             
-    //         return res.status(201).send()
-    //     } catch (error) {
-    //         next(error)
-    //     }
-    // }
+            return res.status(201).send()
+        } catch (error) {
+            next(error)
+        }
+    }
 }
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const UserController = require('./controllers/UserController')
 
 routes.get('/players', UserController.index)
 routes.get('/questions', UserController.questions)
-// routes.post('/players', UserController.createPlayer)
+routes.post('/players', UserController.createPlayer)
+routes.post('/logon', UserController.logon)
 
 module.exports = routes
