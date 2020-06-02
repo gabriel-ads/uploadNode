89f696643561c9ad3013472b29f9d49dec46e0ec
Gabriel <gabriel.alves450@hotmail.com>
2020-06-02 17:13:49 -0300

    adiciona variaveis de ambiente

--- knexfile.js
+++ knexfile.js
@@ -5,9 +5,9 @@ module.exports = {
     development: {
       client: 'pg',
       connection: {
-        database: "show_do_milhao",
-        user: "postgres",
-        password: "123@"
+        database: process.env.DB,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS
       },
       migrations:{
         directory: `${__dirname}/src/database/migrations`
