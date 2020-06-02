46655b3006039da70ad05ae3a5048821a914d381
Gabriel <gabriel.alves450@hotmail.com>
2020-06-01 22:43:25 -0300

    Alterando o dado retornado pelo endpoint

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const routes = express.Router()
 const UserController = require('./controllers/UserController')
 
 routes.get('/players', UserController.index)
-routes.get('/questions', UserController.questions)
+routes.post('/questions', UserController.questions)
 routes.post('/players', UserController.createPlayer)
 routes.post('/logon', UserController.logon)
 
